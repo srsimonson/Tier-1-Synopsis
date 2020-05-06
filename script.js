54e452cef6b5304f532fdb2f5477437960f0f32f
Steve Simonson <srsimonson@gmail.com>
2020-05-06 00:22:02 -0500

    Tier 1 Synopsis complete

--- script.js
+++ script.js
@@ -15,7 +15,40 @@ function addPurchase(){
   $( '#purchaseNameIn' ).val('');
   $( '#purchasePriceIn' ).val('');
   // calculate remainingBudget
-}
+  calculateRemainingBudget();
+  // update DOM
+  displayPurchases()
+} // end addPurchase
+
+function calculateRemainingBudget() {
+  console.log('in calculateRemainingBudget');
+  // loop through purchases array
+  let totalPrices = 0;
+  for (let i=0; i<purchases.length; i++){
+    // for each purchase, add up total of all prices
+    totalPrices += Number(purchases[i].price);
+  } // end for loop
+  console.log('totalPrices', totalPrices );
+  // subtract totalPrices from budget for remainingBudgetOut
+  const remainingBudget = budget - totalPrices;
+  // display remainingBudget
+  let el = $('#remainingBudgetOut');
+  el.empty();
+  el.append( remainingBudget);
+} // end calculateRemainingBudget
+
+function displayPurchases(){
+  console.log( ' in display purchases');
+  // target output by ID
+  let el = $('#purchasesOut');
+  // empty
+  el.empty();
+  // loop through purchases array
+  for( purchase of purchases ){
+    // for each purchase, create a list item
+      el.append( `<li>` + purchase.name + `: $` + purchase.price + `</li>`);
+  } // end for of
+} // end displayPurchases
 
 function readyNow(){
   // display budget
