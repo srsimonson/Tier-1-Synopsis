81b3de5f109ce734b9ccc314b4f4c061264f80cd
Steve Simonson <srsimonson@gmail.com>
2020-05-05 22:57:26 -0500

    synopsis test push

---
+++ script.js
@@ -0,0 +1,28 @@
+$( document ).ready( readyNow );
+
+const budget = 25000;
+let purchases = [];
+function addPurchase(){
+  console.log( 'in add Purchase' );
+  // get user input, create a new object
+  let newPurchase = {
+    name: $('#purchaseNameIn').val(),
+    price: $( '#purchasePriceIn' ).val()
+  }
+  // push the new purchase into the array
+  purchases.push( newPurchase );
+  // empty inputs
+  $( '#purchaseNameIn' ).val('');
+  $( '#purchasePriceIn' ).val('');
+  // calculate remainingBudget
+}
+
+function readyNow(){
+  // display budget
+  // target budgetOut by id
+  let el = $( '#budgetOut');
+  el.empty();
+  el.append( budget );
+  // handle click event
+  $( '#addPurchaseButton').on( 'click', addPurchase );
+} // end readyNow
